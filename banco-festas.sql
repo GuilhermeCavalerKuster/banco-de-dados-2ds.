0e42ed59548de242504ea530a40d84b232217f46
GuilhermeCavalerKuster <guilherme.cavaler.kuster@escola.pr.gov.br>
2025-04-07 09:29:41 -0300

    Create banco-festas.sql

---
+++ banco-festas.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Clientes 
+( 
+); 
+
+CREATE TABLE Festas 
+( 
+); 
+
+CREATE TABLE Pagamento 
+( 
+); 
+
+CREATE TABLE Pix 
+( 
+ Cod_Pix INT,  
+); 
+
+CREATE TABLE Boleto 
+( 
+ Cod_Barras INT,  
+ Data_Vencimento INT,  
+); 
+
+CREATE TABLE CartãoCrédito 
+( 
+ CPF INT,  
+ Endereço INT,  
+ NomeNoCartão INT,  
+); 
+
